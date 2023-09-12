47f62ff009bff35bc77f46008c1d5b687cf24785
BreindelV <144081162+BreindelV@users.noreply.github.com>
2023-09-12 16:51:04 -0600

    Update Main.java

--- Calculadora/src/test/Main.java
+++ Calculadora/src/test/Main.java
@@ -1,16 +1,29 @@
 package test;
 import domain.*;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        double[][] matriz = {
+		Scanner entradaN = new Scanner(System.in);
+	System.out.println("Escriba el tamaño de la matriz cuadrada");
+	int tamanoN = entradaN.nextInt();
+
+	double[][] matriz ={};
+	System.out.println("Escriba los valores de la matriz");
+	for(int i = 0 ; i < tamanoN ; i++){
+		for(int j = 0 ; j < tamanoN ; j++){
+			matriz[i][j] = entradaN.nextDouble();
+		}
+	}
+		
+        /*double[][] matriz = {
                 {1, -1, 2, 3},
                 {2, 1, 0, 1},
                 {3, -1, 1, 2},
                 {2, -1, 0, 1}
-            };
+            };*/
         Calculadora calculadora = new Calculadora();
         
         System.out.println("Det = " + calculadora.calcularDeterminante(matriz));
