d2bc58364c3de84dac640f753415020dde7790ec
Gerardo Villanueva <a22216369@alumnos.uady.mx>
2023-09-11 19:01:38 -0600

    Añadiendo conjunto de funciones preliminares para calcular determinantes de matrices nxn

---
+++ Calculadora/src/domain/Calculadora.java
@@ -0,0 +1,87 @@
+package domain;
+
+public class Calculadora {
+
+	//constructor
+	public Calculadora() {
+		
+	}
+	
+	//metodos
+	
+	public double calcularDeterminante(double matriz[][]){
+		
+		double det = 0;
+		//Si la matriz es 1x1 la det es el valor unico
+		if (matriz.length == 1 && matriz[0].length == 1) {
+			det = matriz[0][0];
+		//Si es 2x2 la det es la diferencia de las diagonales superior e inferior
+		}else if(matriz.length == 2 && matriz[0].length == 2) {
+			det = (matriz[0][0] * matriz[1][1]) - (matriz[0][1] * matriz[1][0]);
+			//System.out.println(det);
+			
+			
+		}else{
+			//Si es mayor a 2x2, realizar el metodo por cofactores
+			det = this.seleccionarFila(matriz);
+		}
+			
+		return det;
+	}
+	
+	private double seleccionarFila(double matriz[][]) {
+		double det_final = 0;
+		//Enviamos un valor de la fila elegida
+		for(int i = 0; i < matriz.length; i++) {
+			//Definimos la det de la submatriz generada
+			
+			
+			double det_submatriz = calcularCofactor(this.disminuirMatriz(matriz, i), i, matriz[i][0]);
+					
+			
+			//Se suma a la final
+			det_final += det_submatriz;
+			//System.out.println(det_final + "\n");
+		}
+		return det_final;
+	}
+	
+	private double[][] disminuirMatriz(double matriz[][], int limite) {
+		int band = 0;
+		//Creamos la submatriz
+		double[][] submatriz = new double[matriz.length-1][matriz[0].length-1];
+		
+	    for (int f = 0, i = 0; f < matriz.length && i < (matriz.length-1); f++, i++){
+	    	
+	    	if(f == limite && band ==0) {
+	    		f++;
+	    		band = 1;
+	    	}
+
+	        for (int c = 1, j = 0; c < matriz[0].length && j < (matriz[0].length-1); c++, j++){
+	            //Llenamos con datos la submatriz
+	        	submatriz[i][j] = matriz[f][c];
+	        }
+	    }
+	    this.imprimirMatriz(submatriz);
+	    return submatriz;
+	}
+	
+	private double calcularCofactor(double matriz[][], int exp, double escalar) {
+		//calcular formula del cofactor
+		double cofactor = Math.pow(-1, exp) * escalar * this.calcularDeterminante(matriz);
+		System.out.println(cofactor + "\n");
+		return cofactor;
+	}
+	
+	
+	public void imprimirMatriz(double matriz[][]) {
+		for(int i = 0; i < matriz.length; i++) {
+			for(int j = 0; j < matriz[i].length; j++) {
+				System.out.print(matriz[i][j] + " ");
+			}
+			System.out.print("\n");
+		}
+		System.out.print("\n");
+	}
+}
--- Calculadora/src/test/Main.java
+++ Calculadora/src/test/Main.java
@@ -1,10 +1,28 @@
 package test;
+import domain.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        double[][] matriz = {
+                {1, -1, 2, 3},
+                {2, 1, 0, 1},
+                {3, -1, 1, 2},
+                {2, -1, 0, 1}
+            };
+        Calculadora calculadora = new Calculadora();
+        
+        System.out.println("Det = " + calculadora.calcularDeterminante(matriz));
+        //calculadora.imprimirMatriz(matriz);
+        
+	}
+	
+	
+	public static double[][] leerMatriz(){
+		double[][] matriz = new double[1][2];
+		
+		return matriz;
 	}
 
 }
