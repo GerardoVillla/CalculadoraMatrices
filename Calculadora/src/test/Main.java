777fdb99830c298d73c481a803e55d0be6a3026b
cr15v <144637940+cr15v@users.noreply.github.com>
2023-09-12 20:39:01 -0600

    Update Main.java

--- Calculadora/src/test/Main.java
+++ Calculadora/src/test/Main.java
@@ -5,37 +5,23 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner entradaN = new Scanner(System.in);
+		
+	Scanner entradaN = new Scanner(System.in);
+	
 	System.out.println("Escriba el tamaño de la matriz cuadrada");
 	int tamanoN = entradaN.nextInt();
-
-	double[][] matriz ={};
+	 double[][] matriz = new double[tamanoN][tamanoN];
 	System.out.println("Escriba los valores de la matriz");
 	for(int i = 0 ; i < tamanoN ; i++){
 		for(int j = 0 ; j < tamanoN ; j++){
 			matriz[i][j] = entradaN.nextDouble();
 		}
 	}
-		
-        /*double[][] matriz = {
-                {1, -1, 2, 3},
-                {2, 1, 0, 1},
-                {3, -1, 1, 2},
-                {2, -1, 0, 1}
-            };*/
-        Calculadora calculadora = new Calculadora();
-        
-        System.out.println("Det = " + calculadora.calcularDeterminante(matriz));
-        //calculadora.imprimirMatriz(matriz);
-        
-	}
-	
-	
-	public static double[][] leerMatriz(){
-		double[][] matriz = new double[1][2];
-		
-		return matriz;
-	}
+   
+	Calculadora calculadora = new Calculadora();
 
+    System.out.println("Det = " + calculadora.calcularDeterminante(matriz));
+    entradaN.close();
+        
+   }
 }
