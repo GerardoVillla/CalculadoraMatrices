c76e8433c94679c9ce102907cc821a075756f94a
cr15v <144637940+cr15v@users.noreply.github.com>
2023-09-12 20:51:24 -0600

    Update Main.java

--- Calculadora/src/test/Main.java
+++ Calculadora/src/test/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		
 	Scanner entradaN = new Scanner(System.in);
 	
 	System.out.println("Escriba el tamano de la matriz cuadrada");
@@ -23,8 +24,9 @@ public class Main {
 	}
    
 	Calculadora calculadora = new Calculadora();
-	System.out.println("\nDet = " + calculadora.calcularDeterminante(matriz));
-	entradaN.close();
+	calculadora.imprimirMatriz(matriz);
+    System.out.println("\nDet = " + calculadora.calcularDeterminante(matriz));
+    entradaN.close();
         
    }
 }
